2013424e74d26e643ab9a214a346ce6125cb029a
loi2003 <loitntse171276@fpt.edu.vn>
2025-06-03 09:57:54 +0700

    fix router

---
+++ src/pages/ConsultationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ConsultationPage = () => {
+  return (
+    <div className="consultation-page">
+      <h1>Consultation Services</h1>
+      <p>Book a consultation with our experts to get personalized health advice and support.</p>
+    </div>
+  );
+};
+
+export default ConsultationPage;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,24 +3,36 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import SignIn from '../components/authentication/SignIn';
 import SignUp from '../components/authentication/SignUp';
+import AboutPage from '../pages/AboutPage';
+// import PregnancyPage from '../pages/PregnancyPage';
+// import NutritionalGuidancePage from '../pages/NutritionalGuidancePage';
+import ConsultationPage from '../pages/ConsultationPage';
+import CommunityPage from '../pages/CommunityPage';
+import DonationPage from '../pages/DonationPage';
+// import FindProviderPage from '../pages/FindProviderPage';
+// import HealthAssessmentPage from '../pages/HealthAssessmentPage';
+// import PrivacyPolicyPage from '../pages/PrivacyPolicyPage';
+// import TermsOfServicePage from '../pages/TermsOfServicePage';
+// import ContactUsPage from '../pages/ContactUsPage';
+// import AssessmentPage from '../pages/AssessmentPage';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/signin" element={<SignIn />} />
     <Route path="/signup" element={<SignUp />} />
-    <Route path="/health-a-z" element={<div>Health A-Z Page</div>} />
-    <Route path="/gender-care" element={<div>Gender Care Page</div>} />
-    <Route path="/conditions" element={<div>Conditions Page</div>} />
-    <Route path="/treatments" element={<div>Treatments Page</div>} />
-    <Route path="/resources" element={<div>Resources Page</div>} />
-    <Route path="/find-provider" element={<div>Find a Provider Page</div>} />
-    <Route path="/health-assessment" element={<div>Health Assessment Page</div>} />
-    <Route path="/about" element={<div>About Us Page</div>} />
-    <Route path="/privacy" element={<div>Privacy Policy Page</div>} />
-    <Route path="/terms" element={<div>Terms of Service Page</div>} />
-    <Route path="/contact" element={<div>Contact Us Page</div>} />
-    <Route path="/assessment" element={<div>Assessment Page</div>} />
+    <Route path="/about" element={<AboutPage />} />
+    {/* <Route path="/pregnancy" element={<PregnancyPage />} />
+    <Route path="/nutritional-guidance" element={<NutritionalGuidancePage />} /> */}
+    <Route path="/consultation" element={<ConsultationPage />} />
+    <Route path="/community" element={<CommunityPage />} />
+    <Route path="/donation" element={<DonationPage />} />
+    {/* <Route path="/find-provider" element={<FindProviderPage />} />
+    <Route path="/health-assessment" element={<HealthAssessmentPage />} />
+    <Route path="/privacy" element={<PrivacyPolicyPage />} />
+    <Route path="/terms" element={<TermsOfServicePage />} />
+    <Route path="/contact" element={<ContactUsPage />} />
+    <Route path="/assessment" element={<AssessmentPage />} /> */}
   </Routes>
 );
 
